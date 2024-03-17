f26fb5a4e27fdde3e0bdd2f33220e9bb8cab5ab1
jacopos86 <simonij@tcd.ie>
2024-03-17 00:07:16 -0400

    update input params

--- pyspinorbitevol/read_input_data.py
+++ pyspinorbitevol/read_input_data.py
@@ -33,14 +33,9 @@ class input_data_class(ABC):
         # phonons
         if 'phonons' in data:
             self.ph = data['phonons']
-        if 'coordinate_file' in data:
-            self.coordinate_file = data['coordinate_file']
-        if 'optimized_coordinate_file' in data:
-            self.optimized_coordinate_file = data['optimized_coordinate_file']
+        # periodic dimension
         if 'periodic_dimension' in data:
             self.D = data['periodic_dimension']
-        if 'nkpts' in data:
-            self.nkpts = data['nkpts']
         if 'lattice_parameter_ang' in data:
             self.latt_ang = data['lattice_parameter_ang']
         if 'unit_cell' in data:
@@ -88,6 +83,9 @@ class psi4_input_data_class(input_data_class):
             log.error(msg)
         data = yaml.load(f, Loader=yaml.Loader)
         f.close()
+        # read shared variables
+        self.read_shared_data(data)
+        assert self.D == 0
         # psi4 section
         if p.sph_basis and 'llist' in data:
             self.llist = data['llist']
@@ -113,6 +111,11 @@ class psi4_input_data_class(input_data_class):
             # basis file name
             if 'basis_file_name' in data:
                 self.basis_file_name = data['basis_file_name']
+            if 'optimized_coordinate_file' in data:
+                self.optimized_coordinate_file = data['optimized_coordinate_file']
+            # coordinate files
+            if 'coordinate_file' in data:
+                self.coordinate_file = data['coordinate_file']
             # multiplicity
             if 'multiplicity' in data:
                 self.multiplicity = data['multiplicity']
@@ -122,7 +125,38 @@ class psi4_input_data_class(input_data_class):
 class QE_input_data_class(input_data_class):
     def __init__(self):
         super().__init__()
-
+        # n. kpt -> along each dimension
+        # D = 0 -> [1,1,1]
+        # D = 1 -> [1,1,nkpt]
+        # D = 2 -> [nkpt,nkpt,1]
+        # D = 3 -> [nkpt,nkpt,nkpt]
+        self.nkpt = 0
+        self.kgr  = []
+    def read_data(self, input_file):
+        try :
+            f = open(input_file)
+        except :
+            msg = "could not find: " + input_file
+            log.error(msg)
+        data = yaml.load(f, Loader=yaml.Loader)
+        f.close()
+        # read shared variables
+        self.read_shared_data(data)
+        #
+        # read data
+        if 'nkpts' in data:
+            self.nkpt = data['nkpt']
+            if self.D == 0:
+                self.kgr = [1, 1, 1]
+            elif self.D == 1:
+                self.kgr = [1, 1, self.nkpt]
+            elif self.D == 2:
+                self.kgr = [self.nkpt, self.nkpt, 1]
+            elif self.D == 3:
+                self.kgr = [self.nkpt, self.nkpt, self.nkpt]
+            else:
+                log.error("Wrong periodic D : " + str(self.D))
+            
 # input parameters object
 arguments = parser.parse_args()
 code = arguments.calc_typ
--- pyspinorbitevol/unit_cell_class.py
+++ pyspinorbitevol/unit_cell_class.py
@@ -21,45 +21,23 @@ import warnings
 # abstract implementation
 class UnitCell_class(ABC):
 	def __init__(self):
-		# primitive vectors must be in angstrom
-		self.prim_vecs = None
 		self.volume = None
-		self.struct = None
-	def set_primitive_vectors(self, primitive_vectors):
-		# primitive vectors
-		self.lattice = Lattice(primitive_vectors)
-		self.a1 = np.array(primitive_vectors[0])
-		self.a2 = np.array(primitive_vectors[1])
-		self.a3 = np.array(primitive_vectors[2])
-		self.prim_vecs = np.array([self.a1, self.a2, self.a3])
-	def set_volume(self):
-		a23 = np.cross(self.a2, self.a3)
-		self.volume = np.dot(self.a1, a23)
-		log.info("unit cell volume= " + str(self.volume) + " Ang^3")
+		self.cell_struct = None
+		self.lattice = None
+		self.rec_lattice = None
+		self.primitive_vect = None
+	def set_volume(self, a1, a2, a3):
+		a23 = np.cross(a2, a3)
+		self.volume = np.dot(a1, a23)
+		log.info("\t unit cell volume = " + str(self.volume) + " Ang^3")
 		# Ang^3
-	def set_rec_vectors(self):
+	def set_primitive_vectors(self):
+		# primitive vectors -> ang
+		self.primitive_vect = np.array(p.lattice_vect)*p.latticep_ang
+		self.lattice = Lattice(self.primitive_vect)
+	def set_rec_lattice(self):
 		self.rec_lattice = self.lattice.reciprocal_lattice
-		# b1 = 2pi a2 x a3 / V (ang^-1)
-		self.b1 = 2.*np.pi*np.cross(self.a2, self.a3) / self.volume
-		# b2 = 2pi a3 x a1 / V
-		self.b2 = 2.*np.pi*np.cross(self.a3, self.a1) / self.volume
-		# b3 = 2pi a1 x a2 / V
-		self.b3 = 2.*np.pi*np.cross(self.a1, self.a2) / self.volume
-		self.rec_vecs = np.array([self.b1, self.b2, self.b3])
-		# unit vectors
-		self.set_rec_versors()
-	def set_rec_versors(self):
-		self.rcv = [None]*3
-		self.rcv[0] = self.b1 / norm_realv(self.b1)
-		self.rcv[1] = self.b2 / norm_realv(self.b2)
-		self.rcv[2] = self.b3 / norm_realv(self.b3)
-  
-#
-# PSI4 derived class
-class psi4_UnitCell_class(UnitCell_class):
-	def __init__(self):
-		super().__init__()
-	def set_structure(self, atomic_structure):
+	def set_cell_structure(self, atomic_structure):
 		# set up the full atomic structure
 		Atomslist = atomic_structure.sites_list.Atomslist
 		species = []
@@ -71,32 +49,39 @@ class psi4_UnitCell_class(UnitCell_class):
 			R = Site.R0
 			coords.append(R)
 		# set up the structure
-		self.struct = Structure(lattice=self.lattice, species=species, coords=coords,
-			charge=0, validate_proximity=True, coords_are_cartesian=True)
+		self.cell_struct = Structure(lattice=self.lattice, species=species, coords=coords,
+			charge=p.charge, validate_proximity=True, coords_are_cartesian=True)
+
+#
+# PSI4 derived class
+class psi4_UnitCell_class(UnitCell_class):
+	def __init__(self):
+		super().__init__()
+		# primitive vectors must be in angstrom
+		self.NNlist = None
 	def set_nn_atoms(self, atomic_structure):
 		Atomslist = atomic_structure.sites_list.Atomslist
 		self.NNlist = []
 		#
 		# periodic dimension 0
 		assert p.D == 0
-		if p.D == 0:
-			# suppress warnings
-			with warnings.catch_warnings():
-				warnings.simplefilter("ignore")
-				NNstruct = CrystalNN()
-				for i in range(len(Atomslist)):
-					nndata = NNstruct.get_nn_info(self.struct, i)
-					# store ONLY first nn in list (weight = 1)
-					# check if the atom is in the structure
-					nndata2 = []
-					for data in nndata:
-						R0 = Atomslist[data['site'].index].R0
-						d = data['site'].coords - R0
-						if norm_realv(d) < eps:
-							#print(i, data['site'].coords, data['site'].index)
-							nndata2.append(data)
-					# store ONLY first nn in list (weight = 1)
-					self.NNlist.append(nndata2)
+		# suppress warnings
+		with warnings.catch_warnings():
+			warnings.simplefilter("ignore")
+			NNstruct = CrystalNN()
+			for i in range(len(Atomslist)):
+				nndata = NNstruct.get_nn_info(self.cell_struct, i)
+				# store ONLY first nn in list (weight = 1)
+				# check if the atom is in the structure
+				nndata2 = []
+				for data in nndata:
+					R0 = Atomslist[data['site'].index].R0
+					d = data['site'].coords - R0
+					if norm_realv(d) < eps:
+						#print(i, data['site'].coords, data['site'].index)
+						nndata2.append(data)
+				# store ONLY first nn in list (weight = 1)
+				self.NNlist.append(nndata2)
 
 # QE cell class
 #
@@ -105,22 +90,23 @@ class QE_UnitCell_class(UnitCell_class):
 	def __init__(self):
 		super().__init__()
 		self.super_cell = None
-	def set_structure(self, atomic_structure):
+	def build_supercell(self, atomic_structure):
 		# set up the full atomic structure
 		Atomslist = atomic_structure.sites_list.Atomslist
+		# atomic species
 		species = []
 		for Site in Atomslist:
 			elem = Site.element
 			species.append(elem)
+		# atom coordinates
 		coords = []
 		for Site in Atomslist:
 			R = Site.R0
 			coords.append(R)
-		# set up the structure
-		self.struct = PhonopyAtoms(cell=p.lattice_vect*p.lattice_param,
-                             scaled_positions=coords, symbols=species)
-	def build_supercell(self):
-		phonon = Phonopy(unitcell=self.struct, supercell_matrix=p.supercell_size)
+		# set up the unit cell
+		unit_cell = PhonopyAtoms(cell=self.primitive_vect,
+                    scaled_positions=coords, symbols=species)
+		phonon = Phonopy(unitcell=unit_cell, supercell_matrix=p.supercell_size)
 		self.super_cell = phonon.get_supercell()
 
 # 
